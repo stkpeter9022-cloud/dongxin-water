ec7410206cd09996c0b302db03c8bfb3176f7827
DI PAN <stkpeter9022@gmail.com>
2026-07-16 02:55:01 +0800

    Add Google Ads tag

--- app/components/Schema.tsx
+++ app/components/Schema.tsx
@@ -2,19 +2,32 @@ export default function Schema() {
   const schema = {
     "@context": "https://schema.org",
     "@type": "Plumber",
+
     name: "棟鑫水電",
-    image: "https://你的網域.com/icon.png",
-    telephone: "0918-808-209",
-    url: "https://你的網域.com",
+
+    url: "https://dongxin24.com",
+
+    image: "https://dongxin24.com/icon.png",
+
+    logo: "https://dongxin24.com/icon.png",
+
+    telephone: "+886918808209",
+
+    description:
+      "棟鑫水電提供雙北24小時水電急修，專業處理跳電維修、漏水抓漏、水管疏通、馬桶不通、抽水馬達、沈水馬達、配電箱維修及室內配線。",
+
     priceRange: "$$",
+
     areaServed: [
       "台北市",
-      "新北市"
+      "新北市",
     ],
+
     address: {
       "@type": "PostalAddress",
-      addressCountry: "TW"
+      addressCountry: "TW",
     },
+
     openingHoursSpecification: [
       {
         "@type": "OpeningHoursSpecification",
@@ -25,15 +38,80 @@ export default function Schema() {
           "Thursday",
           "Friday",
           "Saturday",
-          "Sunday"
+          "Sunday",
         ],
         opens: "00:00",
-        closes: "23:59"
-      }
+        closes: "23:59",
+      },
     ],
+
+    hasOfferCatalog: {
+      "@type": "OfferCatalog",
+      name: "水電服務",
+
+      itemListElement: [
+        {
+          "@type": "Offer",
+          itemOffered: {
+            "@type": "Service",
+            name: "跳電維修",
+          },
+        },
+        {
+          "@type": "Offer",
+          itemOffered: {
+            "@type": "Service",
+            name: "漏水處理",
+          },
+        },
+        {
+          "@type": "Offer",
+          itemOffered: {
+            "@type": "Service",
+            name: "抓漏服務",
+          },
+        },
+        {
+          "@type": "Offer",
+          itemOffered: {
+            "@type": "Service",
+            name: "馬桶不通",
+          },
+        },
+        {
+          "@type": "Offer",
+          itemOffered: {
+            "@type": "Service",
+            name: "水管疏通",
+          },
+        },
+        {
+          "@type": "Offer",
+          itemOffered: {
+            "@type": "Service",
+            name: "抽水馬達",
+          },
+        },
+        {
+          "@type": "Offer",
+          itemOffered: {
+            "@type": "Service",
+            name: "沈水馬達",
+          },
+        },
+        {
+          "@type": "Offer",
+          itemOffered: {
+            "@type": "Service",
+            name: "室內配線",
+          },
+        },
+      ],
+    },
+
     sameAs: [
-      "https://line.me/ti/p/ezn_Ad6UpZ"
-    ]
+      "https://line.me/ti/p/ezn_Ad6UpZ",
+    ],
   };
 
   return (
